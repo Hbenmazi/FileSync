fe6902d6c216af0a6647a4d91e88cf55b58163c2
Hbenmazi <894286780@qq.com>
2020-05-25 18:26:31 +0800

    add ignore

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import sys
 from PyQt5 import QtWidgets
 from ui.UIFileSyncLauncher import UIFileSyncLauncherForm
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    launcher_widget = QtWidgets.QWidget()
-    ui_launcher = UIFileSyncLauncherForm()
-    ui_launcher.setupUi(launcher_widget)
-    launcher_widget.show()
-    sys.exit(app.exec_())
+
+app = QtWidgets.QApplication(sys.argv)
+launcher_widget = QtWidgets.QWidget()
+ui_launcher = UIFileSyncLauncherForm()
+ui_launcher.setupUi(launcher_widget)
+launcher_widget.show()
+sys.exit(app.exec_())
